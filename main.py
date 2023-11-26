089e77c83c656d732c61f3fd231bc538b4b86d0b
AIGom <138079698+jaeyeolppark@users.noreply.github.com>
2023-11-26 23:34:34 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+import streamlit as st 
+import tempfile
+import os
+
+# 제목
+st.title("PDF Summary")
+st.write("---")
+
+# 파일 업로드
+uploaded_file = st.file_uploader("PDF 파일을 올려주세요!", type=['pdf'])
+st.write("---")
+
+def pdf_to_document(uploaded_files):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
+
+# 업로드가 완료되면 동작하는 코드
+if uploaded_file is not None:
+    pages = pdf_to_document(pdf_to_document)
+    pass
+
+    # Split
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 200,
+        chunk_overlap  = 20,
+        length_function = len,
+        is_separator_regex = False,
+    )
+
+    texts = text_splitter.split_documents(pages)
+
+    # Embedding
+    embeddings_model = OpenAIEmbeddings()
+
+    # load it into Chroma
+    db = Chroma.from_documents(texts, embeddings_model)
+
+    # Question
+    st.header("PDF에서 알고 싶은 내용을 물어보세요")
+    question = st.text_input('질문을 입력하세요 :)')
+    
+    if st.button('입력하기'):
+        with st.spinner('Wait for it...'):
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0)
+            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+            result = qa_chain({"query" : question})
+            st.write(result["result"])
